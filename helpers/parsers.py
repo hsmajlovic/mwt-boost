1d4b8b118d9589304cc948735c66e3b3db736a46
harissmajlovic <smajlovicharis@hotmail.com>
2017-02-13 01:32:00 +0100

    Parsers init

---
+++ helpers/parsers.py
@@ -0,0 +1,36 @@
+""" IO parsers module """
+import os
+from typing import Any
+
+import yaml
+
+from utils.enums import FileType
+
+
+def write_to_file(
+        data: Any,
+        path_to_file: str,
+        **kwargs: Any
+) -> None:
+    """
+    Writes the provided data to specified file of specified file format.
+
+    Args:
+        data: - Any writable data.
+        path_to_file: - Path to file in which data is ought to be stored.
+        file_type: - File extension: .yaml, .txt
+        **kwargs: - Any other relevant kwargs to be passed to the actual writing method.
+    """
+
+    file_type = FileType(os.path.splitext(path_to_file)[1])
+
+    if os.path.exists(path_to_file):
+        append_write = 'a'  # append if already exists
+    else:
+        append_write = 'w'  # make a new file if not
+
+    with open(path_to_file, append_write) as outfile:
+        if file_type == FileType.YAML:
+            yaml.dump(data, outfile, **kwargs)
+        elif file_type == FileType.TXT:
+            outfile.write(data)
---
+++ utils/enums.py
@@ -0,0 +1,8 @@
+""" Enum classes module """
+from enum import Enum
+
+
+class FileType(Enum):
+    """ Enum class for file types/extensions """
+    TXT = '.txt'
+    YAML = '.yaml'
